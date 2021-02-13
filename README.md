7d2b1642f2745b2562a5be8a2a9b5fbca6dd2a23
p1131 <panos.st@tuta.io>
2021-02-13 18:03:05 +0200

    Removed trailing spaces and some other minor changes.

--- README.md
+++ README.md
@@ -1,55 +1,63 @@
 Logitech Unifier
 ================
 
-A program to pair devices to a Logitech Unifying Receiver on Linux. Also works for Nano Receiver (Non-unifying)
+A program to pair devices to a Logitech Unifying Receiver on Linux. Also works
+for Nano Receiver (Non-unifying).
 
-Logitech only provides a Windows program to do this, so if you're running Linux, you either need to pair your 
-devices on a windows box or use the unify.c script. 
+Logitech only provides a Windows program to do this, so if you're running
+Linux, you either need to pair your devices on a Windows or use the `unify.c`
+program.
 
-Quick How To - BETA
-----------------
+Usage
+-----
 
-Run:
+1. Compile it:
+```
+gcc -o unify unify.c
+```
 
-    ./autopair.sh
+2. Find your hidraw device. Run the following for every `hidrawX` on your
+   system until you find `HID_NAME=Logitech USB Receiver`:
+```
+cat /sys/class/hidraw/hidraw0/device/uevent
+```
 
-This script which will do a bunch of stuff automatically.
+ If `hidraw0` isn't it, try `hidraw1` and so on. If you have multiple `hidrawX`
+ devices that match, try all of them.
 
-- It will automatically compile the binary (if needed)
-- mark it executable (if needed)
-- find the Logitech device in hidraw
-- run the script with additional prompting (turn off device first)
+3. Pair it:
+```
+sudo ./unify /dev/hidrawX
+```
 
-Longer How To if Above Doesn't Work
---------------------------------
+ Where `X` is the matching number from the previous step.
 
-1\. Compile it
-
-    gcc -o unify unify.c
-
-2\. Find your hidraw device
-
-To find it, run the following for every hidrawX on your system until you find `HID_NAME=Logitech USB Receiver`:
-
-    cat /sys/class/hidraw/hidraw0/device/uevent
-    
-If hidraw0 isn't it, try hidraw1 and so on.
-
-3\. Pair it
-
-Run: 
-
-    sudo ./unify /dev/hidrawX
-
-Where X is the matching number from step 2. 
-
-4\. Turn on your device like the tool will say.
-
-That's it!  Device should work now. 
+4. Turn on your device like the tool will say and that's it! Device should work
+   now.
 
 Thanks
 ------
 
-To Tycho for this article: http://tycho.ws/blog/2011/12/logitech-unifying-receiver
+To Tycho for this article:
+http://tycho.ws/blog/2011/12/logitech-unifying-receiver
 
-And Benjamin for writing the script: https://groups.google.com/forum/?fromgroups#!msg/linux.kernel/zYS6yddI8yU/9cMvg3k9xTYJ
+And Benjamin for writing the program:
+https://groups.google.com/forum/?fromgroups#!msg/linux.kernel/zYS6yddI8yU/9cMvg3k9xTYJ
+
+COPYING
+-------
+
+	Copyright 2011 Benjamin Tissoires <benjamin.tissoires@gmail.com>
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
--- unify.c
+++ unify.c
@@ -1,87 +1,93 @@
-/* 
- * Copyright 2011 Benjamin Tissoires <benjamin.tissoires@gmail.com> 
- * 
- * This program is free software: you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation, either version 3 of the License, or 
- * (at your option) any later version. 
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- * GNU General Public License for more details. 
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
- */ 
 
-#include <linux/input.h> 
-#include <linux/hidraw.h> 
-#include <sys/ioctl.h> 
-#include <fcntl.h> 
-#include <unistd.h> 
-#include <stdio.h> 
-#include <errno.h> 
+/* Copyright 2011 Benjamin Tissoires <benjamin.tissoires@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#define USB_VENDOR_ID_LOGITECH                        (__u32)0x046d 
-#define USB_DEVICE_ID_UNIFYING_RECEIVER                (__s16)0xc52b 
-#define USB_DEVICE_ID_NANO_RECEIVER                (__s16)0xc52f 
-#define USB_DEVICE_ID_NANO_RECEIVER_ALT                (__s16)0xc534 
-#define USB_DEVICE_ID_UNIFYING_RECEIVER_2        (__s16)0xc532 
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, char **argv) 
-{ 
-        int fd; 
-        int res; 
-        struct hidraw_devinfo info; 
-        char magic_sequence[] = {0x10, 0xFF, 0x80, 0xB2, 0x01, 0x00, 0x00}; 
+#include <sys/ioctl.h>
+#include <linux/input.h>
+#include <linux/hidraw.h>
 
-        if (argc == 1) { 
-                errno = EINVAL; 
-                perror("No hidraw device given"); 
-                return 1; 
-        } 
+#define USB_VENDOR_ID_LOGITECH            (__u32)0x046D
+#define USB_DEVICE_ID_UNIFYING_RECEIVER   (__s16)0xC52B
+#define USB_DEVICE_ID_NANO_RECEIVER       (__s16)0xC52F
+#define USB_DEVICE_ID_NANO_RECEIVER_ALT   (__s16)0xC534
+#define USB_DEVICE_ID_UNIFYING_RECEIVER_2 (__s16)0xC532
 
-        /* Open the Device with non-blocking reads. */ 
-        fd = open(argv[1], O_RDWR|O_NONBLOCK); 
-
-        if (fd < 0) { 
-                perror("Unable to open device"); 
-                return 1; 
-        } 
-
-        /* Get Raw Info */ 
-        res = ioctl(fd, HIDIOCGRAWINFO, &info); 
-        if (res < 0) { 
-                perror("error while getting info from device"); 
-        } else { 
-                if (info.bustype != BUS_USB || 
-                    info.vendor != USB_VENDOR_ID_LOGITECH || 
-                    (info.product != USB_DEVICE_ID_UNIFYING_RECEIVER && 
-                     info.product != USB_DEVICE_ID_NANO_RECEIVER && 
-                     info.product != USB_DEVICE_ID_NANO_RECEIVER_ALT && 
-                     info.product != USB_DEVICE_ID_UNIFYING_RECEIVER_2)) { 
-                        errno = EPERM; 
-                        perror("The given device is not a Logitech " 
-                                "Unifying Receiver"); 
-                        return 1; 
-                } 
-        } 
-
-        /* Send the magic sequence to the Device */ 
-        res = write(fd, magic_sequence, sizeof(magic_sequence)); 
-        if (res < 0) { 
-                printf("Error: %d\n", errno); 
-                perror("write"); 
-        } else if (res == sizeof(magic_sequence)) { 
-                printf("The receiver is ready to pair a new device.\n" 
-                "Switch your device on to pair it.\n"); 
-        } else { 
-                errno = ENOMEM; 
-                printf("write: %d were written instead of %ld.\n", res, 
-                        sizeof(magic_sequence)); 
-                perror("write"); 
-        } 
-        close(fd); 
-        return 0; 
-} 
+int main(const int argc, const char *const argv[])
+{
+	int fd;
+	long res;
+	struct hidraw_devinfo info;
+	const unsigned char magic_sequence[] = {
+		0x10,
+		0xFF,
+		0x80,
+		0xB2,
+		0x01,
+		0x00,
+		0x00
+	};
+	if (argc == 1) {
+		errno = EINVAL;
+		perror("No hidraw device given");
+		return 1;
+	}
+	/* Open the Device with non-blocking reads. */
+	fd = open(argv[1], O_RDWR | O_NONBLOCK);
+	if (fd < 0) {
+		perror("Unable to open device");
+		return 1;
+	}
+	/* Get Raw Info */
+	res = ioctl(fd, HIDIOCGRAWINFO, &info);
+	if (res < 0) {
+		perror("error while getting info from device");
+	}
+	else if (info.bustype != BUS_USB ||
+		 info.vendor != USB_VENDOR_ID_LOGITECH ||
+		 (info.product != USB_DEVICE_ID_UNIFYING_RECEIVER &&
+		  info.product != USB_DEVICE_ID_NANO_RECEIVER &&
+		  info.product != USB_DEVICE_ID_NANO_RECEIVER_ALT &&
+		  info.product != USB_DEVICE_ID_UNIFYING_RECEIVER_2))
+	{
+		errno = EPERM;
+		perror("The given device is not a Logitech Unifying Receiver");
+		close(fd);
+		return 1;
+	}
+	/* Send the magic sequence to the Device */
+	res = write(fd, magic_sequence, sizeof(magic_sequence));
+	if (res < 0) {
+		fprintf(stderr, "Error: %d\n", errno);
+		perror("write");
+	}
+	else if (res != sizeof(magic_sequence)) {
+		errno = ENOMEM;
+		fprintf(stderr, "write: %ld were written instead of %lu.\n",
+		       res, sizeof(magic_sequence));
+		perror("write");
+	}
+	else {
+		puts("The receiver is ready to pair a new device.\n"
+		     "Switch your device (off and) on to pair it.");
+	}
+	close(fd);
+	return 0;
+}
