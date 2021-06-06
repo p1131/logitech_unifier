85e85a46cd4f248efede4bd0b54bb2eb950c7c28
p1131 <p1131@xyz.xyz>
2021-06-06 20:41:41 +0300

    Minor changes.

--- unify.c
+++ unify.c
@@ -17,12 +17,12 @@
 
 #include <errno.h>
 #include <fcntl.h>
-#include <stdio.h>
-#include <unistd.h>
-
-#include <sys/ioctl.h>
 #include <linux/input.h>
 #include <linux/hidraw.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
 #define USB_VENDOR_ID_LOGITECH            (__u32)0x046D
 #define USB_DEVICE_ID_UNIFYING_RECEIVER   (__s16)0xC52B
@@ -30,12 +30,9 @@
 #define USB_DEVICE_ID_NANO_RECEIVER_ALT   (__s16)0xC534
 #define USB_DEVICE_ID_UNIFYING_RECEIVER_2 (__s16)0xC532
 
-int main(const int argc, const char *const argv[])
+int main(int argc, char *argv[])
 {
-	int fd;
-	long res;
-	struct hidraw_devinfo info;
-	const unsigned char magic_sequence[] = {
+	static const unsigned char magic_seq[] = {
 		0x10,
 		0xFF,
 		0x80,
@@ -44,50 +41,53 @@ int main(const int argc, const char *const argv[])
 		0x00,
 		0x00
 	};
+	int fd, ret = EXIT_FAILURE;
 	if (argc == 1) {
 		errno = EINVAL;
 		perror("No hidraw device given");
-		return 1;
+		exit(ret);
 	}
 	/* Open the Device with non-blocking reads. */
 	fd = open(argv[1], O_RDWR | O_NONBLOCK);
 	if (fd < 0) {
-		perror("Unable to open device");
-		return 1;
+		perror("open() failed");
+		exit(ret);
 	}
 	/* Get Raw Info */
-	res = ioctl(fd, HIDIOCGRAWINFO, &info);
-	if (res < 0) {
-		perror("error while getting info from device");
-	}
-	else if (info.bustype != BUS_USB ||
-		 info.vendor != USB_VENDOR_ID_LOGITECH ||
-		 (info.product != USB_DEVICE_ID_UNIFYING_RECEIVER &&
-		  info.product != USB_DEVICE_ID_NANO_RECEIVER &&
-		  info.product != USB_DEVICE_ID_NANO_RECEIVER_ALT &&
-		  info.product != USB_DEVICE_ID_UNIFYING_RECEIVER_2))
 	{
-		errno = EPERM;
-		perror("The given device is not a Logitech Unifying Receiver");
-		close(fd);
-		return 1;
+		struct hidraw_devinfo info;
+		const int res = ioctl(fd, HIDIOCGRAWINFO, &info);
+		if (res < 0)
+			perror("error while getting info from device");
+		else if (info.bustype != BUS_USB ||
+				info.vendor != USB_VENDOR_ID_LOGITECH ||
+				(info.product != USB_DEVICE_ID_UNIFYING_RECEIVER &&
+				 info.product != USB_DEVICE_ID_NANO_RECEIVER &&
+				 info.product != USB_DEVICE_ID_NANO_RECEIVER_ALT &&
+				 info.product != USB_DEVICE_ID_UNIFYING_RECEIVER_2)) {
+			errno = EPERM;
+			perror("The given device is not a Logitech Unifying Receiver");
+			goto end;
+		}
 	}
 	/* Send the magic sequence to the Device */
-	res = write(fd, magic_sequence, sizeof(magic_sequence));
-	if (res < 0) {
-		fprintf(stderr, "Error: %d\n", errno);
-		perror("write");
+	{
+		const ssize_t res = write(fd, magic_seq, sizeof(magic_seq));
+		if (res < 0)
+			perror("write failed()");
+		else if (res != sizeof(magic_seq)) {
+			errno = ENOMEM;
+			perror("write failed()");
+			fprintf(stderr, "%ld were written instead of %lu.\n",
+					res, sizeof(magic_seq));
+		}
+		else {
+			puts("The receiver is ready to pair a new device.\n"
+				 "Switch your device (off and) on to pair it.");
+			ret = EXIT_SUCCESS;
+		}
 	}
-	else if (res != sizeof(magic_sequence)) {
-		errno = ENOMEM;
-		fprintf(stderr, "write: %ld were written instead of %lu.\n",
-		       res, sizeof(magic_sequence));
-		perror("write");
-	}
-	else {
-		puts("The receiver is ready to pair a new device.\n"
-		     "Switch your device (off and) on to pair it.");
-	}
+end:
 	close(fd);
-	return 0;
+	exit(ret);
 }
